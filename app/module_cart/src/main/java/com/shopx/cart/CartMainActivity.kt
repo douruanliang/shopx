6e757100f5b17542ee8a82bf8a4dd5ea1937e411
douruanliang <douruanliang@meishubao.com>
2021-06-18 14:14:04 +0800

    简化配置参数

---
+++ app/module_cart/src/main/java/com/shopx/cart/CartMainActivity.kt
@@ -0,0 +1,11 @@
+package com.shopx.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cart_activity_main)
+    }
+}
